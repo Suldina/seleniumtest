26463150d6a6e0e5a4713150d3116690cb527134
suldinaelena <elenasuldina@mail.ru>
2017-10-13 14:37:46 +0300

    try

--- src/main/java/Steps/Steps.java
+++ src/main/java/Steps/Steps.java
@@ -2,8 +2,6 @@ package Steps;
 
 
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -11,73 +9,97 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.MainPage;
+import pages.MenPage;
+import pages.SearchResultPage;
 
 import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 
 public class Steps {
     String productId="a[data-product-id='39004581']";
 
-
+    String itemToSearch = "Kensington";
+//
     private WebDriver driver = CHDriver.getWebDriver();
-//    ChromeDriver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-    public void click (String css){
-        driver.findElement(By.cssSelector(css)).click();
-    }
-    public void setValue (String css, String value) {
-        driver.findElement(By.cssSelector(css)).sendKeys(value);
-    }
-    public void getValue (String css){
-        driver.findElement(By.cssSelector(css)).getText();
-    }
-
-    public void findElementByCss (String css){
-//        driver.findElement(By.cssSelector(css));
-        WebElement dynamicElement = (new WebDriverWait(driver, 5))
-                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(css)));
-
-    }
-//    public void findListOfElements(String css) {
-//        List<WebElement> dynamicElements = (new WebDriverWait(driver, 5))
-//                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(css)));
+//////    ChromeDriver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+////    public void click (String css){
+////        driver.findElement(By.cssSelector(css)).click();
+////    }
+////    public void setValue (String css, String value) {
+////        driver.findElement(By.cssSelector(css)).sendKeys(value);
+////    }
+////    public void getValue (String css){
+////        driver.findElement(By.cssSelector(css)).getText();
+////    }
+////
+////    public void findElementByCss (String css){
+//////        driver.findElement(By.cssSelector(css));
+////        WebElement dynamicElement = (new WebDriverWait(driver, 5))
+////                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(css)));
+////
+////    }
+//////    public void findListOfElements(String css) {
+//////        List<WebElement> dynamicElements = (new WebDriverWait(driver, 5))
+//////                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(css)));
+//////    }
+//
+//
+//
+//    @Before
+//    public void preset(String[] args) {
+//        driver.navigate().to("https://ru.burberry.com/");
+//
+////        driver.get();
+//
 //    }
-
-
-
-    @Before
-    public void preset(String[] args) {
-        driver.navigate().to("https://ru.burberry.com/");
-
-//        driver.get();
-
-    }
-    @After
-    public void close() {
-
-        driver.quit();
-    }
+//    @After
+//    public void close() {
+//
+//        driver.quit();
+//    }
     @Test
-    public void testPage() {
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        String searchResult = ".products";
-        String searchCSS = ".main-form__search-box";
-        String btnAddTobag = ".ga-add-to-bag";
-        String btnSearch =".ga-sslu-store-button";
-        String imageCSS ="product-image-asset-content.asset-container.cell-asset-bg.js-asset-container.js-product-cursor-container";
+    public void shouldCheckPDPforItem() throws InterruptedException {
+////        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        String searchResult = ".products";
+//        String searchCSS = ".main-form__search-box";
+//        String btnAddTobag = ".ga-add-to-bag";
+//        String btnSearch =".ga-sslu-store-button";
+//        String imageCSS ="product-image-asset-content.asset-container.cell-asset-bg.js-asset-container.js-product-cursor-container";
 //        findElementByCss(searchCSS);
 //        WebElement element = driver.findElement(By.cssSelector(searchcss))
 //        findElement.sendKeys(fieldToSearch);
 //        dynamicElement.sendKeys(Keys.ENTER);
-        String fieldToSearch = "Kensington";
-        setValue(searchCSS, fieldToSearch);
-        setValue(searchCSS, "Keys.Enter");
-        findElementByCss(searchResult);
-        findElementByCss(productId);
-        click(productId);
-        findElementByCss(btnAddTobag);
-        findElementByCss(imageCSS);
+//        String fieldToSearch = "Kensington";
+//        setValue(searchCSS, fieldToSearch);
+//        setValue(searchCSS, "Keys.Enter");
+//        findElementByCss(searchResult);
+//        findElementByCss(productId);
+//
+//        findElementByCss(btnAddTobag);
+//        findElementByCss(imageCSS);
+        MainPage mainPage = new MainPage(driver);
+        mainPage.open();
+        mainPage.click(By.linkText("Мужчины"));
+//        MenPage menPage = new mainPage.;
+        sleep(3000);
+        mainPage.moveToElement(By.id("navigation-bar-link-cat1350556"));
+        sleep(3000);
+        mainPage.click(By.cssSelector("a[data-category-id='cat3650040']"));
+        sleep(1000);
+        mainPage.moveToElement(By.cssSelector("div[id='wai-material']"));
+        sleep(1500);
+        mainPage.click(By.cssSelector(".facet-checkbox-label "));
+        mainPage.click(By.linkText("Показать результаты"));
 
 
+//        SearchResultPage searchResultPage = mainPage.searchForItem(itemToSearch);
+     //   assertThat(searchResultPage.getSearchResultsCount().toLowerCase(), is("37"));
+//        searchResultPage.header.selectFirstMenu();
+     //   searchResultPage.selectFirstProduct();
+        driver.quit();
     }
 
 
---
+++ src/main/java/pages/MenPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class MenPage extends FBasePage {
+
+    public static final String PRODUCT_ITEM_CSS = ".product-card.js-product-card";
+    public static final String SEARCH_RESULTS_COUNT_CSS = ".search-meta_results-number.type-h4.js-shelf-section";
+
+    public MenPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void selectFirstProduct() {
+        List<WebElement> items = driver.findElements(By.cssSelector(PRODUCT_ITEM_CSS));
+        items.get(0).click();
+    }
+
+    public String getSearchResultsCount() {
+        return driver.findElement(By.cssSelector(SEARCH_RESULTS_COUNT_CSS)).getText().trim();
+    }
+
+
+}
